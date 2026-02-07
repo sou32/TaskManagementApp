8fbcb76c4bfc7d7ef36601616b43748b82dd8536
sou32 <osasou0329@gmail.com>
2026-02-07 12:30:14 +0900

    テストプロジェクトの追加

---
+++ TaskManagementTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagementApp.UseCase;
+namespace TaskManagementTest;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("0", false)]
+    [InlineData("1", true)]
+    [InlineData("5", true)]
+    [InlineData("6", false)]
+    [InlineData("あ", false)]
+    [InlineData("abc", false)] 
+    [InlineData("", false)]
+    public void Test1(string input, bool expected)
+    {
+        IHomeValidation homeValidation = new HomeValidation();
+        Assert.Equal(expected, homeValidation.Validation(input));
+    }
+}
